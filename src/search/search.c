a7a9d16f115996f6073da11f86418af2782cae7d
Zander Brown <zbrown@gnome.org>
2019-06-06 12:43:33 +0100

    search: Skip disabled providers, sort them

--- src/search/search.c
+++ src/search/search.c
@@ -41,7 +41,69 @@ phosh_search_class_init (PhoshSearchClass *klass)
   object_class->finalize = phosh_search_finalize;
 }
 
-// Don't rely on settings or key, they are null when called due to app change
+// Sort algorithm taken straight from remoteSearch.js, comments and all
+static int
+sort_providers (gconstpointer a,
+                gconstpointer b,
+                gpointer      user_data)
+{
+  GAppInfo *app_a = NULL;
+  GAppInfo *app_b = NULL;
+  const char *app_id_a = NULL;
+  const char *app_id_b = NULL;
+  GStrv order = user_data;
+  int idx_a = -1;
+  int idx_b = -1;
+  int i = 0;
+
+  g_return_val_if_fail (PHOSH_IS_SEARCH_PROVIDER ((gpointer) a), -1);
+  g_return_val_if_fail (PHOSH_IS_SEARCH_PROVIDER ((gpointer) b), -1);
+
+  g_object_get ((gpointer) a, "app-info", &app_a, NULL);
+  g_object_get ((gpointer) b, "app-info", &app_b, NULL);
+
+  g_return_val_if_fail (G_IS_APP_INFO (app_a), -1);
+  g_return_val_if_fail (G_IS_APP_INFO (app_b), -1);
+
+  app_id_a = g_app_info_get_id (app_a);
+  app_id_b = g_app_info_get_id (app_b);
+
+  while ((order[i])) {
+    if (idx_a == -1 && g_strcmp0 (order[i], app_id_a) == 0) {
+      idx_a = i;
+    }
+
+    if (idx_b == -1 && g_strcmp0 (order[i], app_id_b) == 0) {
+      idx_b = i;
+    }
+
+    if (idx_a != -1 && idx_b != -1) {
+      break;
+    }
+
+    i++;
+  }
+
+  // if no provider is found in the order, use alphabetical order
+  if ((idx_a == -1) && (idx_b == -1)) {
+    return g_utf8_collate (g_app_info_get_name (app_a),
+                           g_app_info_get_name (app_b));
+  }
+
+  // if providerA isn't found, it's sorted after providerB
+  if (idx_a == -1)
+    return 1;
+
+  // if providerB isn't found, it's sorted after providerA
+  if (idx_b == -1)
+    return -1;
+
+  // finally, if both providers are found, return their order in the list
+  return (idx_a - idx_b);
+}
+
+
+// Don't rely on settings or key, they are null when called for an app change
 static void
 reload_providers (GSettings   *settings,
                   char        *key,
@@ -51,7 +113,20 @@ reload_providers (GSettings   *settings,
   const char *const *data_dirs = g_get_system_data_dirs ();
   const char *data_dir = NULL;
   g_autoptr (GHashTable) provider_set = NULL;
+  g_autolist (PhoshSearchProvider) providers = NULL;
+  g_auto (GStrv) enabled = NULL;
+  g_auto (GStrv) disabled = NULL;
+  g_auto (GStrv) sort_order = NULL;
   int i = 0;
+
+  if (g_settings_get_boolean (priv->settings, "disable-external")) {
+    // TODO: Clear all
+    return;
+  }
+
+  enabled = g_settings_get_strv (priv->settings, "enabled");
+  disabled = g_settings_get_strv (priv->settings, "disabled");
+  sort_order = g_settings_get_strv (priv->settings, "sort-order");
 
   provider_set = g_hash_table_new_full (g_str_hash,
                                         g_str_equal,
@@ -186,16 +261,40 @@ reload_providers (GSettings   *settings,
         default_disabled = default_disabled_tmp;
       }
 
+      if (!default_disabled) {
+        if (g_strv_contains ((const char * const*) disabled, desktop_id)) {
+          g_debug ("Provider %s has been disabled", provider);
+          continue;
+        }
+      } else {
+        if (!g_strv_contains ((const char * const*) enabled, desktop_id)) {
+          g_debug ("Provider %s hasn't been enabled", provider);
+          continue;
+        }
+      }
 
       provider_object = phosh_search_provider_new (desktop_id,
                                                    bus_path,
                                                    bus_name,
                                                    autostart,
                                                    default_disabled);
-      g_object_ref (provider_object);
+
+      providers = g_list_prepend (providers, g_object_ref (provider_object));
 
       g_hash_table_add (provider_set, g_strdup (bus_path));
     }
+  }
+
+  providers = g_list_sort_with_data (providers, sort_providers, sort_order);
+
+  providers = g_list_copy_deep (providers, (GCopyFunc) g_object_ref, NULL);
+
+  for (GList *l = providers; l; l = l->next) {
+    GAppInfo *info = NULL;
+
+    g_object_get (l->data, "app-info", &info, NULL);
+
+    g_print ("%25s %s\n", g_app_info_get_name (info), g_app_info_get_id (info));
   }
 }
 
