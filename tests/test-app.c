836e40a386ed75d5233a6db2a563f8344d5cec33
Zander Brown <zbrown@gnome.org>
2019-07-09 14:15:15 +0100

    test: set size info on the PhoshApp

--- tests/test-app.c
+++ tests/test-app.c
@@ -10,7 +10,15 @@ static void
 test_phosh_app_new(void)
 {
   PhoshApp *app = PHOSH_APP (phosh_app_new ("com.example.foo", "bar"));
+  g_object_set (app,
+                "win-width", 360,
+                "win-height", 640,
+                "max-height", 445,
+                "max-width", 240,
+                NULL);
   g_assert (app);
+  g_assert (PHOSH_IS_APP (app));
+  g_assert (GTK_IS_WIDGET (app));
   g_assert_cmpstr (phosh_app_get_app_id (app), ==, "com.example.foo");
   g_assert_cmpstr (phosh_app_get_title (app), ==, "bar");
   gtk_widget_destroy (GTK_WIDGET (app));
